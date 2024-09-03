7f219f495d1da9aa264f351c0a9da7983ee12219
Gabriel Henrique Mombelli <gabriel.mombelli9@gmail.com>
2024-09-02 22:09:54 -0300

    Atividade 10

---
+++ src/CalculoDeDescontoProgressivo.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class CalculoDeDescontoProgressivo {
+    public static void main(String[] args) {
+
+
+        Double valor = 0.0;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Qual o valor da compra? ");
+        valor = sc.nextDouble();
+
+        if (valor > 500) {
+            System.out.println(valor * 0.8);
+        }else if (valor >= 200 && valor <= 500) {
+            System.out.println(valor * 0.9);
+        } else if (valor >= 100 && valor <= 199) {
+            System.out.println(valor * 0.95);
+        }else
+            {System.out.println(valor);
+        }
+
+    }
+}
+
+
